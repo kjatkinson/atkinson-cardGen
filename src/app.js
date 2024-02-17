4b0d60df87479e872c607202a884ab0d4b4eea90
kjatkinson <149319433+kjatkinson@users.noreply.github.com>
2024-02-17 01:48:15 +0000

    need to fixjs/html

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //randomness for suit and number
+  const suits = ["♦", "♥", "♠", "♣"];
+  const randomSuitIndex = Math.floor(Math.random() * suits.length);
+  const randomSuit = suits[randomSuitIndex];
+  console.log(randomSuit);
+
+  const values = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  const randomValueIndex = Math.floor(Math.random() * values.length);
+  const randomValue = values[randomValueIndex];
+  console.log(randomValue);
+
+  //rows and insert icons/value
+  const topRow = document.querySelector(".topRow");
+  const middleRow = document.querySelector(".middleRow");
+  const bottomRow = document.querySelector(".bottomRow");
+
+  const topTextNode = document.createElement("p");
+  const middleTextNode = document.createElement("p");
+  const bottomTextNode = document.createElement("p");
+
+  topRowElement.appendChild(topTextNode);
+  topRow.appendChild(topRowElement);
+
+  middleRowElement.appendChild(middleTextNode);
+  middleRow.appendChild(middleRowElement);
+
+  bottomRowElement.appendChild(bottomTextNode);
+  bottomRow.appendChild(bottomRowElement);
 };
