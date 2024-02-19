4e5302d63f1d4a1e8d50ff8028a7b283f828e6bf
kjatkinson <149319433+kjatkinson@users.noreply.github.com>
2024-02-19 01:06:14 +0000

    card body added

--- src/app.js
+++ src/app.js
@@ -3,28 +3,41 @@ window.onload = function() {
   const suits = ["♦", "♥", "♠", "♣"];
   const randomSuitIndex = Math.floor(Math.random() * suits.length);
   const randomSuit = suits[randomSuitIndex];
-  console.log(randomSuit);
 
   const values = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
   const randomValueIndex = Math.floor(Math.random() * values.length);
   const randomValue = values[randomValueIndex];
-  console.log(randomValue);
 
   //rows and insert icons/value
   const topRow = document.querySelector(".topRow");
   const middleRow = document.querySelector(".middleRow");
   const bottomRow = document.querySelector(".bottomRow");
+  window.onload = function() {
+    //randomness for suit and number
+    const suits = ["♦", "♥", "♠", "♣"];
+    const randomSuitIndex = Math.floor(Math.random() * suits.length);
+    const randomSuit = suits[randomSuitIndex];
 
-  const topTextNode = document.createElement("p");
-  const middleTextNode = document.createElement("p");
-  const bottomTextNode = document.createElement("p");
+    const values = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+    const randomValueIndex = Math.floor(Math.random() * values.length);
+    const randomValue = values[randomValueIndex];
 
-  topRowElement.appendChild(topTextNode);
-  topRow.appendChild(topRowElement);
+    //rows and insert icons/value
+    const topRow = document.querySelector(".topRow");
+    const middleRow = document.querySelector(".middleRow");
+    const bottomRow = document.querySelector(".bottomRow");
 
-  middleRowElement.appendChild(middleTextNode);
-  middleRow.appendChild(middleRowElement);
+    const topTextNode = document.createTopElement("p");
+    const middleTextNode = document.createMiddleElement("p");
+    const bottomTextNode = document.createBottomElement("p");
 
-  bottomRowElement.appendChild(bottomTextNode);
-  bottomRow.appendChild(bottomRowElement);
+    topRow.appendChild("topElement");
+    topRow.appendChild("topRowElement");
+
+    // middleRowElement.appendChild(middleTextNode);
+    //middleRow.appendChild(middleRowElement);
+
+    //bottomRowElement.appendChild(bottomTextNode);
+    //bottomRow.appendChild(bottomRowElement);
+  };
 };
